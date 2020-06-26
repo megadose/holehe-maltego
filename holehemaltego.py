d2bfc2cd1c5cffff3a2dcc41bf68af1a742f1edf
megadose <51822495+megadose@users.noreply.github.com>
2020-06-26 18:00:37 +0200

    Add files via upload

---
+++ holehemaltego.py
@@ -0,0 +1,34 @@
+from MaltegoTransform import *
+import json,requests
+emailT=sys.argv[1]
+import os
+trx = MaltegoTransform()
+from holehe import *
+import random
+import string
+
+websites=[adobe,ebay,apple,facebook,instagram,tumblr,pastebin,github,twitter,pinterest,lastfm,spotify,firefox]
+for website in websites:
+    infos=website(emailT)
+    i=str(website).split(" ")[1].split(" ")[0]
+    if infos["exists"]==True:
+        web = trx.addEntity("megadose."+i,"Found")
+        if infos["emailrecovery"]!= None:
+            email = trx.addEntity("maltego.EmailAddress",infos["emailrecovery"])
+            email.setLinkLabel("Found in "+i)
+        if infos["phoneNumber"]!= None:
+            email = trx.addEntity("maltego.PhoneNumber",infos["phoneNumber"])
+            email.setLinkLabel("Found in "+i)
+        if infos["others"]!= None:
+            if "@" not in infos["others"]["FullName"]:
+                email = trx.addEntity("maltego.Person",infos["others"]["FullName"])
+                email.setLinkLabel("Found in "+i)
+            reandom =''.join(random.choice(string.ascii_lowercase) for i in range(30))
+            params = (
+                ('source', 'indexpage'),
+                ('url', infos["others"]["profilePicture"]),
+                ('alias', reandom),
+            )
+            response = requests.get('https://tinyurl.com/create.php', params=params)
+            web.setIconURL("https://tinyurl.com/"+reandom)
+print(trx.returnOutput())
